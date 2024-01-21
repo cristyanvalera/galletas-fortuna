f7e03bbfdef816af4d58409efe42eae3e50403d1
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-01-21 13:37:02 -0400

    add css styles

--- src/components/RandomPhrase.jsx
+++ src/components/RandomPhrase.jsx
@@ -2,7 +2,7 @@ export const RandomPhrase = ({ quote }) => {
     const { author, phrase } = quote;
 
     return (
-        <div>
+        <div className="content">
             <p><q>{phrase}</q></p>
             <p><small>Fuente: {author}</small></p>
         </div>

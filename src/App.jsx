0117c0f96756d0ad08c18fce40f9f4587acd4330
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-01-21 20:17:37 -0400

    support for change image background when phrase change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { useState } from 'react';
+import bgList from "./utils/bgList.json";
 import phrases from './utils/phrases.json';
 import randomIndex from './services/randomIndex';
 import { RandomPhrase } from './components/RandomPhrase';
@@ -7,15 +8,27 @@ import { ButtonPhrase } from './components/ButtonPhrase';
 
 function App() {
 	const sentence = randomIndex(phrases);
+	const bgIndex = randomIndex(bgList);
+
 	const [quote, setQuote] = useState(sentence);
+	const [bgImgRandom, setBgImgRandom] = useState(bgIndex);
+
+	const bgStyle = {
+		backgroundImage: `url('../assets/fondo${bgImgRandom}.jpg')`,
+	};
 
 	return (
-		<div className="container">
-			<h1>Galletas de la fortuna</h1>
+		<div className="app" style={bgStyle}>
+			<div className="container">
+				<h1>Galletas de la fortuna</h1>
 
-			<ButtonPhrase setQuote={setQuote} />
+				<ButtonPhrase
+					setQuote={setQuote}
+					setBgImgRandom={setBgImgRandom}
+				/>
 
-			<RandomPhrase quote={quote} />
+				<RandomPhrase quote={quote} />
+			</div>
 		</div>
 	);
 }
--- src/components/ButtonPhrase.jsx
+++ src/components/ButtonPhrase.jsx
@@ -1,9 +1,11 @@
 import randomIndex from "../services/randomIndex";
 import phrases from './../utils/phrases.json';
+import bgList from './../utils/bgList.json';
 
-export const ButtonPhrase = ({ setQuote }) => {
+export const ButtonPhrase = ({ setQuote, setBgImgRandom }) => {
     const handleClick = () => {
         setQuote(randomIndex(phrases));
+        setBgImgRandom(randomIndex(bgList));
     };
 
     return (
